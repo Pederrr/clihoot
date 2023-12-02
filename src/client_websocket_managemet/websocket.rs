1599b5d3bc28f183fac6fe545b96aa1d0b96d44a
Pavol Kycina <xkycina@fi.muni.cz>
2023-12-02 10:53:15 +0100

    subsribers are now functioning, fixed race condition with wait()

--- src/client_websocket_managemet/websocket.rs
+++ src/client_websocket_managemet/websocket.rs
@@ -3,13 +3,14 @@ use actix::{Actor, Context, Message};
 
 use futures::stream::{SplitSink, SplitStream};
 use futures::task::SpawnExt;
-use futures::{SinkExt, StreamExt};
+use futures::{Sink, SinkExt, StreamExt};
 use std::cell::RefCell;
 use std::sync::{Arc, Mutex};
 
 use tokio::net::TcpStream;
 use tokio_tungstenite::MaybeTlsStream;
 use tokio_tungstenite::{connect_async, tungstenite, WebSocketStream};
+use tokio_tungstenite::tungstenite::connect;
 
 // message used for request to send something server, this message should be passed to websocket actor
 #[derive(Message)]
@@ -31,17 +32,15 @@ pub(crate) struct Subscribe(pub Recipient<MessageFromServer>);
 // just subscribe for incoming messages
 pub struct WebsocketActor {
     ws_stream_tx: Arc<
-        Mutex<
-            RefCell<
-                SplitSink<
-                    WebSocketStream<MaybeTlsStream<TcpStream>>,
-                    tungstenite::protocol::Message,
-                >,
+        RefCell<
+            SplitSink<
+                WebSocketStream<MaybeTlsStream<TcpStream>>,
+                tungstenite::protocol::Message,
             >,
         >,
     >,
     ws_stream_rx: Arc<RefCell<SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>>>,
-    subscribers: Vec<Recipient<MessageFromServer>>,
+    subscribers: Arc<Mutex<RefCell<Vec<Recipient<MessageFromServer>>>>>,
 }
 
 impl WebsocketActor {
@@ -53,8 +52,8 @@ impl WebsocketActor {
 
         WebsocketActor {
             ws_stream_rx: Arc::new(RefCell::new(rx)),
-            ws_stream_tx: Arc::new(Mutex::new(RefCell::new(tx))),
-            subscribers: vec![],
+            ws_stream_tx: Arc::new(RefCell::new(tx)),
+            subscribers: Arc::new(Mutex::new(RefCell::new(vec![]))),
         }
     }
 }
@@ -66,19 +65,16 @@ impl Handler<WebsocketMsg> for WebsocketActor {
     fn handle(&mut self, msg: WebsocketMsg, ctx: &mut Context<Self>) {
         let ws_stream = Arc::clone(&self.ws_stream_tx);
 
-        // TODO: maybe remove Mutex
         async move {
             println!("Client websocket actor: sending message");
             ws_stream
-                .lock()
-                .unwrap()
                 .borrow_mut()
                 .send(tokio_tungstenite::tungstenite::Message::Text(msg.0))
                 .await
                 .unwrap();
         }
         .into_actor(self)
-        .spawn(ctx);
+        .wait(ctx);
     }
 }
 
@@ -87,7 +83,7 @@ impl Handler<Subscribe> for WebsocketActor {
 
     fn handle(&mut self, msg: Subscribe, _: &mut Self::Context) {
         println!("Client websocket actor: subscribing");
-        self.subscribers.push(msg.0);
+        self.subscribers.lock().unwrap().borrow_mut().push(msg.0);
     }
 }
 
@@ -98,21 +94,20 @@ impl Actor for WebsocketActor {
         println!("Websocket actor is alive");
 
         let ws_stream_rx = Arc::clone(&self.ws_stream_rx);
-
-        // TODO: here we are cloning an old value, new subscribers do not get subscribed
-        //  -> probably use a mutex/lock/Arc or Arc<Mutex<>> to share the subscribers
-        let subscribers = self.subscribers.clone();
+        let subscribers_clone = Arc::clone(&self.subscribers);
 
         async move {
             // listen for messages from server
             while let Ok(incoming_msg) = ws_stream_rx.borrow_mut().next().await.unwrap() {
                 let incoming_msg_text = incoming_msg.to_text().unwrap().to_string();
 
+                let subscribers_locked = subscribers_clone.lock().unwrap();
+
                 println!(
                     "message arrived, sending to all {} subscribers",
-                    subscribers.len()
+                    subscribers_locked.borrow().len()
                 );
-                for sub in &subscribers {
+                for sub in subscribers_locked.borrow().iter() {
                     sub.send(MessageFromServer {
                         content: incoming_msg_text.clone(),
                     })
